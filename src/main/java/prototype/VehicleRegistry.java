831db1101f163ee55800a5a0d3548d8081bff9cb
Mariana Salas <157374989+MariSalas23@users.noreply.github.com>
2025-02-16 17:33:06 -0500

    Update VehicleRegistry.java

--- src/main/java/prototype/VehicleRegistry.java
+++ src/main/java/prototype/VehicleRegistry.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VehicleRegistry {
+    // Este mapa actúa como un registro donde se guardan diferentes prototipos de vehículos
     private Map<String, VehiclePrototype> prototypes = new HashMap<>();
 
+    // Método que permite agregar un prototipo de vehículo al registro
     public void addPrototype(String key, VehiclePrototype vehicle) {
         prototypes.put(key, vehicle);
     }
 
+    // Método que permite obtener una copia clonada de un prototipo de vehículo a partir de su clave
     public VehiclePrototype getClone(String key) {
         return prototypes.get(key).clone();
     }
